ca5caedafe3e7aad65e9c6986c25c2bdb86d93bb
Nikola Krsmanovic <nikolakrsmanovic342@gmail.com>
2023-06-02 16:24:21 +0200

    first commit

---
+++ 4EIT_B2 domace zivotinje/java/java.js
@@ -0,0 +1,45 @@
+var krava = document.querySelector("#audio1");
+var patka = document.querySelector("#audio2");
+var pile = document.querySelector("#audio3");
+var pas = document.querySelector("#audio4");
+var macka = document.querySelector("#audio5");
+function Pusti1() {
+	krava.pause();
+	patka.pause();
+	pile.pause();
+	pas.pause();
+	macka.pause();
+	pas.play();
+}
+function Pusti2() {
+	krava.pause();
+	patka.pause();
+	pile.pause();
+	pas.pause();
+	macka.pause();
+	patka.play();
+}
+function Pusti3() {
+	krava.pause();
+	patka.pause();
+	pile.pause();
+	pas.pause();
+	macka.pause();
+	macka.play();
+}
+function Pusti4() {
+	krava.pause();
+	patka.pause();
+	pile.pause();
+	pas.pause();
+	macka.pause();
+	pile.play();
+}
+function Pusti5() {
+	krava.pause();
+	patka.pause();
+	pile.pause();
+	pas.pause();
+	macka.pause();
+	krava.play();
+}
